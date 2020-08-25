fcf86845fad97ba129ca36403625d40400be2027
Custer, Cameron <Cameron.Custer@mines.sdsmt.edu>
2020-08-25 16:39:51 -0600

    m0010 - [ci skip] - initial setup

---
+++ m0010/network.h
@@ -0,0 +1,9 @@
+#ifndef _NETWORK_H_
+#define _NETWORK_H_
+
+// Enumerated type starting at 0
+enum networkType { INVALID, CLASSA, APRIVATE, LOCALHOST, CLASSB, BPRIVATE, CLASSC, CPRIVATE, CLASSD, CLASSE };
+
+// Two typedef's created for short int and unsigned int
+typedef unsigned short int octet;
+typedef unsigned int ip;
