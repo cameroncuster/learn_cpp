068d1889e8f8ac837ce761d974fc0d1749610219
Cameron Custer <Cameron.Custer@mines.sdsmt.edu>
2020-10-11 22:55:57 -0600

    m0080 a - [ci skip] - SEGSEV error is fatal

--- m0080/TestCases.cpp
+++ m0080/TestCases.cpp
@@ -19,3 +19,29 @@ TEST_CASE( "destructor" )
     REQUIRE( list1.empty( ) == true );
     REQUIRE( list1.size( ) == 0 );
 }
+
+TEST_CASE( "find" )
+{
+    sortedSingle list1;
+    REQUIRE( list1.find( 10 ) == false );
+}
+
+TEST_CASE( "retrievePosition" )
+{
+    sortedSingle list1;
+    REQUIRE( list1.retrievePosition( 10 ) == 0 );
+}
+
+TEST_CASE( "insert" )
+{
+    sortedSingle list1;
+    REQUIRE( list1.insert( 10 ) == true );
+    CHECK( list1.empty( ) == false );
+    CHECK( list1.size( ) == 1 );
+    REQUIRE( list1.insert( 30 ) == true );
+    CHECK( list1.empty( ) == false );
+    CHECK( list1.size( ) == 2 );
+    REQUIRE( list1.insert( 20 ) == true );
+    CHECK( list1.empty( ) == false );
+    CHECK( list1.size( ) == 3 );
+}
