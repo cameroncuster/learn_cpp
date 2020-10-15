0cd17764a46736077b644b5fa45468a62c7cf97d
Cameron Custer <Cameron.Custer@mines.sdsmt.edu>
2020-10-15 13:39:48 -0600

    m0080 - [ci skip] - copy constructor written and working

--- m0080/SortedSingled.cpp
+++ m0080/SortedSingled.cpp
@@ -5,25 +5,18 @@
  *****************************************************************************/
 #include "sortedSingle.h"
 
-sortedSingle::sortedSingle( sortedSingle &l)
+sortedSingle::sortedSingle( sortedSingle &l )
 {
-    // temp val to walk through
-    node *temp = headptr;
-
-    // set a new headptr equal to the first node in the new list
-    node *newheadptr = temp;
-
-    // walk through the list allocating memory dynamically for each node in the new list
-    while( temp != nullptr )
+    if( headptr = nullptr )
     {
-        // set each value in the new list equal to the value in the old list
-        node *newval = new ( nothrow ) node;
-        if( newval == nullptr )
-        {
-            exit( 0 );
-        }
-        newval = temp;
-        newval->next = temp->next;
-        temp = temp->next;
+        return;
+    }
+    headptr = l.headptr;
+    curr = headptr;
+    while( l.curr != nullptr )
+    {
+        curr->theItem = l.curr->theItem;
+        curr->next = l.curr->next;
+        l.curr = l.curr->next;
     }
 }
--- m0080/sortedSingle.h
+++ m0080/sortedSingle.h
@@ -57,4 +57,5 @@ class sortedSingle
         node *next;     /*!< pointer to the remaining nodes in list */
     };
     node *headptr;      /*!< The location of the first node in the list */
+    node *curr = nullptr;
 };
