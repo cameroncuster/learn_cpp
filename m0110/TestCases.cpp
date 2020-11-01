d4b92b6d6bdf048e5af1b67580d7fe476238d7b6
Cameron Custer <Cameron.Custer@mines.sdsmt.edu>
2020-11-01 15:24:47 -0700

    m0110 b - [ci skip] - test case for remove has been written now I can write code

--- m0110/TestCases.cpp
+++ m0110/TestCases.cpp
@@ -14,7 +14,7 @@ using namespace std;
 void printQueue( Queue q, ostream &out );
 
 //write your test cases here
-TEST_CASE( "QUEUE::ENQUEUE - inserting into a queue" )
+TEST_CASE( "QUEUE::ENQUEUE - INSERTING INTO A QUEUE" )
 {
     Queue queue1;
     ostringstream sout1;
@@ -28,7 +28,7 @@ TEST_CASE( "QUEUE::ENQUEUE - inserting into a queue" )
         REQUIRE( sout1.str( ) == "" );
     }
 
-    // insert items in the queue
+    // enqueue items in the queue
     REQUIRE( queue1.enqueue( "a" ) == true );
     REQUIRE( queue1.enqueue( "b" ) == true );
     REQUIRE( queue1.enqueue( "c" ) == true );
@@ -43,13 +43,95 @@ TEST_CASE( "QUEUE::ENQUEUE - inserting into a queue" )
     // print
     queue1.print( sout1 );
 
-    SECTION( "MANY ITEMS" )
+    SECTION( "MULTIPLE ITEM ENQUEUE" )
     {
         CHECK( queue1.size( ) == 10 );
         REQUIRE( sout1.str( ) == "a, b, c, d, e, f, g, h, i, j" );
     }
 }
 
+TEST_CASE( "QUEUE::DEQUEUE - REMOVING FROM A QUEUE" )
+{
+    Queue queue1;
+    ostringstream sout1;
+
+    // words
+    string word1 = "a";
+    string word2 = "b";
+    string word3 = "c";
+    string word4 = "d";
+    string word5 = "e";
+    string word6 = "f";
+    string word7 = "g";
+    string word8 = "h";
+    string word9 = "i";
+
+    // empty print
+    queue1.print( sout1 );
+
+    SECTION( "EMPTY" )
+    {
+        CHECK( queue1.size( ) == 0 );
+        REQUIRE( sout1.str( ) == "" );
+    }
+
+    // enqueue single item
+    CHECK( queue1.enqueue( word1 ) == true );
+
+    // dequeue the single item
+    REQUIRE( queue1.dequeue( word1 ) == true );
+
+    // print
+    queue1.print( sout1 );
+
+    SECTION( "SINGLE ITEM DEQUEUE" )
+    {
+        CHECK( queue1.size( ) == 0 );
+        REQUIRE( sout1.str( ) == "" );
+    }
+
+    // enqueue multiple items
+    CHECK( queue1.enqueue( word1 ) == true );
+    CHECK( queue1.enqueue( word2 ) == true );
+    CHECK( queue1.enqueue( word3 ) == true );
+    CHECK( queue1.enqueue( word4 ) == true );
+    CHECK( queue1.enqueue( word5 ) == true );
+    CHECK( queue1.enqueue( word6 ) == true );
+    CHECK( queue1.enqueue( word7 ) == true );
+    CHECK( queue1.enqueue( word8 ) == true );
+    CHECK( queue1.enqueue( word9 ) == true );
+
+    // dequeue half items
+    REQUIRE( queue1.dequeue( word1 ) == true );
+    REQUIRE( queue1.dequeue( word2 ) == true );
+    REQUIRE( queue1.dequeue( word3 ) == true );
+    REQUIRE( queue1.dequeue( word4 ) == true );
+    REQUIRE( queue1.dequeue( word5 ) == true );
+
+    // print
+    queue1.print( sout1 );
+
+    SECTION( "MULTIPLE ITEM DEQUEUE" )
+    {
+        CHECK( queue1.size( ) == 4 );
+        REQUIRE( sout1.str( ) == "f, g, h, i" );
+    }
+
+    // dequeue the remaining items
+    REQUIRE( queue1.dequeue( word6 ) == true );
+    REQUIRE( queue1.dequeue( word7 ) == true );
+    REQUIRE( queue1.dequeue( word8 ) == true );
+    REQUIRE( queue1.dequeue( word9 ) == true );
+
+    // print
+    queue1.print( sout1 );
+
+    SECTION( "MULTIPLE ITEM DEQUEUE NULLIFY" )
+    {
+        CHECK( queue1.size( ) == 0 );
+        REQUIRE( sout1.str( ) == "" );
+    }
+}
 
 
 
