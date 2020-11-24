16a9dffd90767867ada457ac45a6c5ee058037f0
Cameron Custer <Cameron.Custer@mines.sdsmt.edu>
2020-11-24 14:09:36 -0700

    m1301 - [ci skip] - no default constructor from inheritance

--- m1301/Testcases.cpp
+++ m1301/Testcases.cpp
@@ -1,3 +1,85 @@
 #include "..\\catch.hpp"
 #include "sortedSingle.h"
 using namespace std;
+
+TEST_CASE( "ITERATOR" )
+{
+    sortedSingle<int> list1;
+    sortedSingle<int>::iterator it;
+
+    ostringstream sout1;
+
+    list1.insert( 10 );
+    list1.insert( 30 );
+    list1.insert( 20 );
+    list1.insert( 50 );
+    list1.insert( 40 );
+
+    it = list1.begin( );
+
+    SECTION( "PRE_INCREMENT" )
+    {
+        while( it != list1.end( ) )
+        {
+            sout1 << *it;
+            ++it;
+            if( it != list1.end( ) )
+                sout1 << ", ";
+        }
+        REQUIRE( sout1.str( ) == "10, 20, 30, 40, 50" );
+    }
+
+    SECTION( "PRE_INCREMENT" )
+    {
+        while( it != list1.end( ) )
+        {
+            sout1 << *it;
+            it++;
+            if( it != list1.end( ) )
+                sout1 << ", ";
+        }
+        REQUIRE( sout1.str( ) == "10, 20, 30, 40, 50" );
+    }
+
+    SECTION( "EQUALITY&!" )
+    {
+        sortedSingle<int>::iterator it1 = list1.begin( );
+        sortedSingle<int>::iterator it2 = list1.begin( );
+        sortedSingle<int>::iterator it3 = list1.end( );
+
+        REQUIRE( it1 == it2 );
+        REQUIRE( it1 != it3 );
+        REQUIRE( it2 != it3 );
+    }
+
+    SECTION( "DEREFRENCE" )
+    {
+        it = list1.begin( );
+        *it = 9;
+
+        for( it = list1.begin( ); it != list1.end( ); it++ )
+            sout1 << *it << ' ';
+        REQUIRE( sout1.str( ) == "9 20 30 40 50 " );
+    }
+
+    SECTION( "DEREFRENCE_RETURN_VAL" )
+    {
+        int n;
+        n = *it;
+        REQUIRE( n == 10 );
+        n = *it++;
+        REQUIRE( n == 10 );
+    }
+
+    SECTION( "ADDRESS_OF_ITEM" )
+    {
+        it = list1.begin( );
+        ++it;
+        it++;
+
+        *it = 35;
+        for( it = list1.begin( ); it != list1.end( ); ++it )
+            sout1 << *it << ' ';
+        REQUIRE( sout1.str( ) == "10 20 35 40 50 " );
+    }
+}
