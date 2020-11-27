60f117fdeab645a7f1104634e70dbd8f5a836d85
Cameron Custer <Cameron.Custer@mines.sdsmt.edu>
2020-11-27 14:56:24 -0700

    m0120 - [ci skip] - real project naming scheme corrected from m1201

--- m0120/TestCases.cpp
+++ m0120/TestCases.cpp
@@ -2,13 +2,11 @@
 #include "sortedSingle.h"
 using namespace std;
 
-TEST_CASE( "TEMPLATE CLASS" )
+TEST_CASE( "INTTEMPLATE" )
 {
     sortedSingle<int> intlist;
-    sortedSingle<string> stringlist;
 
     ostringstream intsout;
-    ostringstream stringsout;
 
     // fill the lists
     REQUIRE( intlist.insert( 10 ) == true );
@@ -17,18 +15,11 @@ TEST_CASE( "TEMPLATE CLASS" )
     REQUIRE( intlist.insert( 40 ) == true );
     REQUIRE( intlist.insert( 50 ) == true );
 
-    REQUIRE( stringlist.insert( "a" ) == true );
-    REQUIRE( stringlist.insert( "b" ) == true );
-    REQUIRE( stringlist.insert( "c" ) == true );
-    REQUIRE( stringlist.insert( "d" ) == true );
-    REQUIRE( stringlist.insert( "e" ) == true );
 
     SECTION( "PRINT COMPARE" )
     {
         intlist.print( intsout );
-        stringlist.print( stringsout );
         REQUIRE( intsout.str( ) == "10, 20, 30, 40, 50" );
-        REQUIRE( stringsout.str( ) == "a, b, c, d, e" );
     }
 
     // find and retrieve the positions of some items
@@ -48,6 +39,42 @@ TEST_CASE( "TEMPLATE CLASS" )
     REQUIRE( intlist.retrievePosition( 50 ) == 5 );
     REQUIRE( intlist.retrievePosition( 60 ) == 0 );
 
+    // remove the items from the list and print
+    REQUIRE( intlist.remove( 00 ) == false );
+    REQUIRE( intlist.remove( 10 ) == true );
+    REQUIRE( intlist.remove( 50 ) == true );
+    REQUIRE( intlist.remove( 20 ) == true );
+    REQUIRE( intlist.remove( 30 ) == true );
+    REQUIRE( intlist.remove( 40 ) == true );
+    REQUIRE( intlist.remove( 10 ) == false );
+    
+    SECTION( "PRINT COMPARE" )
+    {
+        intlist.print( intsout );
+        REQUIRE( intsout.str( ) == "" );
+    }
+}
+
+TEST_CASE( "STRTEMPLATE" )
+{
+    sortedSingle<string> stringlist;
+
+    ostringstream stringsout;
+
+    // fill the lists
+    REQUIRE( stringlist.insert( "a" ) == true );
+    REQUIRE( stringlist.insert( "b" ) == true );
+    REQUIRE( stringlist.insert( "c" ) == true );
+    REQUIRE( stringlist.insert( "d" ) == true );
+    REQUIRE( stringlist.insert( "e" ) == true );
+
+    SECTION( "PRINT COMPARE" )
+    {
+        stringlist.print( stringsout );
+        REQUIRE( stringsout.str( ) == "a, b, c, d, e" );
+    }
+
+    // find and retrieve the positions of some items
     REQUIRE( stringlist.find( "x" ) == false );
     REQUIRE( stringlist.find( "a" ) == true );
     REQUIRE( stringlist.find( "b" ) == true );
@@ -65,14 +92,6 @@ TEST_CASE( "TEMPLATE CLASS" )
     REQUIRE( stringlist.retrievePosition( "y" ) == 0 );
 
     // remove the items from the list and print
-    REQUIRE( intlist.remove( 00 ) == false );
-    REQUIRE( intlist.remove( 10 ) == true );
-    REQUIRE( intlist.remove( 50 ) == true );
-    REQUIRE( intlist.remove( 20 ) == true );
-    REQUIRE( intlist.remove( 30 ) == true );
-    REQUIRE( intlist.remove( 40 ) == true );
-    REQUIRE( intlist.remove( 10 ) == false );
-    
     REQUIRE( stringlist.remove( "x" ) == false );
     REQUIRE( stringlist.remove( "a" ) == true );
     REQUIRE( stringlist.remove( "b" ) == true );
@@ -83,9 +102,7 @@ TEST_CASE( "TEMPLATE CLASS" )
 
     SECTION( "PRINT COMPARE" )
     {
-        intlist.print( intsout );
         stringlist.print( stringsout );
-        REQUIRE( intsout.str( ) == "" );
         REQUIRE( stringsout.str( ) == "" );
     }
 }
